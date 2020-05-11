5013fe2018e7c92f1d7f8d0338872d0e37c5673d
vishwas1 <vishwasanand.cse@gmail.com>
2020-05-11 18:05:34 +0530

    for quickstart

---
+++ hs-script/hs-plugin-install.sh
@@ -0,0 +1,47 @@
+## reference https://stackoverflow.com/a/28938235/1851064
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+BLUE_BG='\033[44m'
+NC='\033[0m' # No Color
+
+KCBASE='/opt/jboss/keycloak'
+
+# exit on any error
+# set -e
+
+echo -e "${BLUE_BG}Uncompressing...${NC}"
+tar -xvzf hs-authenticator.tar.gz 
+cd hs-authenticator
+tar -xvzf hs-theme.tar.gz
+
+
+echo -e "${BLUE_BG}Cleaning the hypersign plugin..${NC}"
+rm -rf ${KCBASE}/hs-plugin-keycloak-ejb-0.2-SNAPSHOT.jar
+rm -rf ${KCBASE}/modules/hs-plugin-keycloak-ejb/
+rm -rf ${KCBASE}/standalone/configuration/hypersign.properties
+
+echo -e "${BLUE_BG}Coping the plugin..${NC}"
+cp hs-plugin-keycloak-ejb-0.2-SNAPSHOT.jar ${KCBASE}
+
+
+echo -e "${BLUE_BG}Dploying the hypersign theme..${NC}"
+cp hs-themes/hypersign-config.ftl ${KCBASE}/themes/base/login
+cp hs-themes/hypersign.ftl ${KCBASE}/themes/base/login
+cp hs-themes/hypersign-new.ftl ${KCBASE}/themes/base/login
+
+
+echo -e "${BLUE_BG}Dploying the hypersign config file..${NC}"
+cp  hypersign.properties ${KCBASE}/standalone/configuration/
+
+
+echo -e "${BLUE_BG}Deploying the hypersign plugin..${NC}"
+cd ${KCBASE}
+sh bin/jboss-cli.sh --command="module add --name=hs-plugin-keycloak-ejb --resources=${KCBASE}/hs-plugin-keycloak-ejb-0.2-SNAPSHOT.jar --dependencies=org.keycloak.keycloak-common,org.keycloak.keycloak-core,org.keycloak.keycloak-services,org.keycloak.keycloak-model-jpa,org.keycloak.keycloak-server-spi,org.keycloak.keycloak-server-spi-private,javax.ws.rs.api,javax.persistence.api,org.hibernate,org.javassist,org.liquibase,com.fasterxml.jackson.core.jackson-core,com.fasterxml.jackson.core.jackson-databind,com.fasterxml.jackson.core.jackson-annotations,org.jboss.resteasy.resteasy-jaxrs,org.jboss.logging,org.apache.httpcomponents,org.apache.commons.codec,org.keycloak.keycloak-wildfly-adduser"
+
+sleep 50
+
+echo -e "${BLUE_BG}Adding hs module to the keycloak configuration${NC}"
+sh bin/jboss-cli.sh --connect --controller=localhost:9990 --command='/subsystem=keycloak-server/:write-attribute(name=providers,value=["classpath:${jboss.home.dir}/providers/*","module:hs-plugin-keycloak-ejb"])'
+
+exit
---
+++ hypersign-setup.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+
+## default parameters
+VERSION="v0.0.1"
+REDIRECT_URI="http://localhost:8000"
+CLIENT_ALIAS="sample-node-js-client"
+NO_PASSWORDLESS=0
+KC_USERNAME=admin
+KC_PASSWORD=admin
+
+usage() {
+    echo "Hypersign-setup runs and configure all required docker containers for integrating your client project with Hypersign authenticaion module"
+    echo  
+    echo "Syntax: hypersign-setup.sh -r <REDIRECT_URI> -a <CLIENT_ALIAS> -u <KC_USERNAME> -p <KC_PASSWORD>"
+    echo "Command line options:"
+    echo "    -r    | --redirect-uri  URI     Redirect URI once the user is authenticated from Hypersign"
+    echo "    -a    | --client-alias  ALIAS   Name of client you want to configure"
+    echo "    -npls | --no-password-less      For legacy authentication method"
+    echo "    -u    | --username              Provide keycloak username"
+    echo "    -p    | --password              Provide keycloak password"
+    echo "    -V    | --version               Print current version"
+    echo "    -h    | --help                  Print this help menu"
+    echo 
+    echo "Example:"
+    echo "    Configure Hypersign for client 'my-demo-client' running on localhost at port 8000"
+    echo "        `basename $0` -r http://localhost:8000 -a my-demo-client -u admin -p admin"
+    echo 
+}
+
+## Warning for default parameters
+if [ $# -eq 0 ]
+then
+    echo "WARNING: No arguments passed"
+    echo "      Default parameters:"
+    echo "          REDIRECT_URI:     ${REDIRECT_URI}"
+    echo "          CLIENT_ALIAS:     ${CLIENT_ALIAS}"
+    echo "          NO_PASSWORD_LESS: ${NO_PASSWORDLESS} ( 0 for password less, 1 for password )"
+    echo "          KC_USERNAME:      ${KC_USERNAME}"
+    echo "          KC_PASSWORD:      ${KC_PASSWORD}"
+    echo
+
+    while true; do
+        read -p "Do you wish to continue with default paramaters? (y|n) " yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+fi
+
+while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
+  -V | --version )
+    echo "hypersign-setup ${VERSION}"
+    exit
+    ;;
+  -h | --help )
+    usage
+    exit
+    ;;
+  -r | --redirect-uri )
+    shift; REDIRECT_URI=$1
+    ;;
+  -a | --client-alias )
+    shift; CLIENT_ALIAS=$1
+    ;;
+  -u | --username )
+    shift; KC_USERNAME=$1
+    ;;
+  -p | --password )
+    shift; KC_PASSWORD=$1
+    ;;
+  -npls | --no-password-less )
+    echo "You have opted for legacy authention mechanism"
+    shift; NO_PASSWORDLESS=1
+    ;;
+  * ) # incorrect option
+    echo "Error: Invalid option"
+    exit;;
+esac; shift; done
+if [[ "$1" == '--' ]]; then shift; fi
+
+
+
+
+## check for docker
+DOCKER_CHECK=$(docker -v)
+if [ -z  "${DOCKER_CHECK}" ]
+then
+    echo "Error: Docker is not installed. Please install docker before proceeding"
+    echo
+    exit
+fi
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+BLUE_BG='\033[44m'
+YELLOW='\033[0;33m'  # Yellow
+NC='\033[0m' # No Color
+CONFIGPATH="/opt/jboss"
+rm -rf dist
+mkdir dist
+
+echo -e "${BLUE_BG}Restarting docker containers...${NC}"
+docker-compose -f docker-compose.yml config
+docker-compose -f docker-compose.yml down
+docker-compose -f docker-compose.yml up -d --remove-orphans
+
+echo -e "${BLUE_BG}Copying required files into keycloak container...${NC}"
+# docker cp hs-script/kc-webcontext.sh "$(docker-compose -f docker-compose.yml ps -q keycloak)":$CONFIGPATH/kc-webcontext.sh
+docker cp hs-script/hs-plugin-install.sh "$(docker-compose -f docker-compose.yml ps -q keycloak)":$CONFIGPATH/hs-plugin-install.sh
+docker cp hs-script/kc-configuration.sh  "$(docker-compose -f docker-compose.yml ps -q keycloak)":$CONFIGPATH/kc-configuration.sh
+
+docker cp data-template/authenticator-flow-update.json "$(docker-compose -f docker-compose.yml ps -q keycloak)":$CONFIGPATH/authenticator-flow-update.json
+docker cp data-template/client-create.json  "$(docker-compose -f docker-compose.yml ps -q keycloak)":$CONFIGPATH/client-create.json
+docker cp data-template/client-update.json  "$(docker-compose -f docker-compose.yml ps -q keycloak)":$CONFIGPATH/client-update.json
+
+echo -e "${BLUE_BG}Running keycloak web context script${NC}"
+# docker-compose -f docker-compose.yml exec --user root keycloak sh $CONFIGPATH/kc-webcontext.sh
+
+if [ ${NO_PASSWORDLESS} -eq 0 ]
+then
+  wget https://github.com/hypermine-bc/hs-authenticator/releases/download/v1.0.1/hs-authenticator.tar.gz -O dist/hs-authenticator.tar.gz
+  docker cp dist/hs-authenticator.tar.gz "$(docker-compose -f docker-compose.yml ps -q keycloak)":$CONFIGPATH/hs-authenticator.tar.gz
+
+  echo -e "${BLUE_BG}Running hypersign keycloak setup script${NC}"
+  docker-compose -f docker-compose.yml exec  keycloak sh $CONFIGPATH/hs-plugin-install.sh
+
+  echo -e "${BLUE_BG}Restarting keycloak server to apply changes${NC}"
+  docker-compose -f docker-compose.yml restart keycloak
+  docker ps
+
+  sleep 50
+else
+  echo "--no-password-less option is set. So skipping hypersign plugin installation"
+  sleep 30
+fi
+
+echo -e "${BLUE_BG}Running hypersign keycloak setting script${NC}"
+docker-compose -f docker-compose.yml exec --user root keycloak sh $CONFIGPATH/kc-configuration.sh ${REDIRECT_URI} ${CLIENT_ALIAS} ${NO_PASSWORDLESS} ${KC_USERNAME} ${KC_PASSWORD}
+
+echo -e "${BLUE_BG}Cleanup${NC}"
+docker-compose -f docker-compose.yml exec --user root keycloak rm -rf hs-authenticator* && rm -rf /opt/jboss/*.json
+rm -rf dist
+exit
+
+
